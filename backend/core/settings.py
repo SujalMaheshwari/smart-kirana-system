cd1a520bb717f1a1576f2e55995af2f1eb769928
SujalMaheshwari <sujalmaheshwari47@gmail.com>
2026-04-24 21:17:56 +0530

    Fix WhiteNoise middleware - use WhiteNoiseMiddleware instead of WhiteNoisePy3Middleware

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoisePy3Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
